3c3352d24906f8ce7bfe35117c595a2c0ad617f8
scokpius <scokpius@gmail.com>
2021-07-13 19:17:30 +0300

    tests the functionality of the function to solve the problem of the presence of a duplicate character in a string

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,37 +1,60 @@
-import java.util.regex.Matcher;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import java.util.regex.Pattern;
 
 public class Line {
-    private final String string_final= "[A-Za-z1-9]";
     private char[] charArray;
+
+    private boolean line;
+
+    public boolean getLine() {
+        return this.line;
+    }
+
+
+    private static final Logger LOGGER = LogManager.getLogger(Line.class);
+
     public Line(String string) {
+       checkStringParameters(string);
+    }
 
+    private void checkStringParameters(String string) {
         charArray = string.toCharArray();
-        try {
-            checkLatinCharactersOrNot(string);
-            searchForSingleCharacters(charArray);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        checkLatinCharactersOrNot(string);
+        if (checkIfTheStringIsEmpty(string) == true &&
+                 searchForSingleCharacters(charArray) == true) {
+             LOGGER.debug("The string consists of Latin characters and / or numbers and does not contain duplicate characters");
+               this.line = true;
+        } else {this.line = false;}
 
     }
 
-    private void checkLatinCharactersOrNot(String string) throws Exception {
+    private boolean checkIfTheStringIsEmpty(String string){
+        if (string == "") {
+            LOGGER.debug("There are no Latin characters and numbers in the string");
+            return false;
+        }else return true;
+
+    }
+
+    private void checkLatinCharactersOrNot(String string){
         Pattern p = Pattern.compile("^[a-zA-Z0-9]*$");
-        if(!p.matcher(string).matches())
-            throw new Exception("There are no Latin characters and numbers in the string");
-
+        if(!p.matcher(string).matches()){
+            LOGGER.debug("There are no Latin characters and numbers in the string");
+        }
     }
 
-
-    private void searchForSingleCharacters(char[] charArray) throws Exception {
+    private boolean searchForSingleCharacters(char[] charArray){
         for(int i = 0; i< charArray.length-1; i++){
             for (int j = i+1; j< charArray.length; j++){
                 if(charArray[i] == charArray[j]){
-                    throw new Exception("There are duplicate characters in the string");
+                    LOGGER.debug("There are duplicate characters in the string");
+                    return false;
                 }
             }
         }
+        return true;
     }
 
 
---
+++ src/test/java/LineTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class LineTest {
+
+    private Line line;
+    private boolean result;
+
+    public LineTest(Line line, boolean result) {
+        this.line = line;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> primeNumbers(){
+        return Arrays.asList(new Object[][] {
+                {new Line(""), false},
+                {new Line("hfbcnd73l8sw"), true},
+                {new Line("тркнмтакаму"), false},
+                {new Line("+-09ипеноо"), false},
+        });
+    }
+    @Test
+    public void checkingForRepetitions() {
+       Assert.assertEquals(result, line.getLine());
+    }
+
+}
